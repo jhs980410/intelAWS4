ccc84d29e3123ece150c3814202893fc0ab94211
jhs980410 <skc05096@naver.com>
2024-12-13 16:51:46 +0900

    학원 컴퓨터에서 이클립스로파일을 업로드 하려고 함

---
+++ 컴퓨터에서 만든 폴더/VarBassic34.java
@@ -0,0 +1,18 @@
+
+public class VarBassic34 {
+
+	public static void main(String[] args) {
+		char ch = 'j';
+		String name = "java";
+		System.out.println();
+		System.out.println();
+		ch = '킹';
+		name = "포스트";
+		System.out.println(ch);
+		System.out.println(name);
+		
+			
+		
+	}
+
+}
